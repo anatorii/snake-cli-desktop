4890aa24a1f1f56eb8a3f899cd26f5be9dbc0158
anatorii <anatoly.igumnov@gmail.com>
2026-06-22 04:16:41 +0200

    snake lib test & gcov

---
+++ src/tests_snake/test_snake.hpp
@@ -0,0 +1,6 @@
+#ifndef TEST_SNAKE_H
+#define TEST_SNAKE_H
+
+#include "../brick_game/snake/lib.hpp"
+
+#endif
---
+++ src/tests_snake/test_snake_lib.cpp
@@ -0,0 +1,73 @@
+#include <gtest/gtest.h>
+#include <string>
+#include <ncurses.h>
+#include "test_snake.hpp"
+#include "../gui/cli/utils.h"
+
+using namespace std;
+
+void run_fsm(int reps = 50) {
+    for (size_t i = 0; i < reps; i++) {
+        GameInfo_t game_info = updateCurrentState();
+    }
+}
+
+TEST(snake_tests, start_test) {
+    int act = -1;
+
+    act = input_to_action('\n');
+    userInput((UserAction_t)act, 0);
+    run_fsm();
+}
+
+TEST(snake_tests, apple_test) {
+    int act = -1;
+
+    act = input_to_action('\n');
+    userInput((UserAction_t)act, 0);
+    run_fsm(200);
+}
+
+TEST(snake_tests, speed_up_test) {
+    int act = -1;
+
+    act = input_to_action('\n');
+    userInput((UserAction_t)act, 0);
+    run_fsm();
+
+    act = input_to_action(32);
+    userInput((UserAction_t)act, 0);
+    run_fsm(100);
+}
+
+TEST(snake_tests, rotation_test) {
+    int act = -1;
+
+    act = input_to_action('\n');
+    userInput((UserAction_t)act, 0);
+    run_fsm();
+
+    act = input_to_action(KEY_LEFT);
+    userInput((UserAction_t)act, 0);
+    run_fsm(100);
+
+    act = input_to_action(KEY_DOWN);
+    userInput((UserAction_t)act, 0);
+    run_fsm(100);
+
+    act = input_to_action(KEY_RIGHT);
+    userInput((UserAction_t)act, 0);
+    run_fsm(100);
+
+    act = input_to_action(KEY_UP);
+    userInput((UserAction_t)act, 0);
+    run_fsm(100);
+}
+
+TEST(snake_tests, wall_collision_test) {
+    int act = -1;
+
+    act = input_to_action('\n');
+    userInput((UserAction_t)act, 0);
+    run_fsm(2000);
+}
